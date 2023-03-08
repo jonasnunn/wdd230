99af657b1c505dae885c76a277c732c35081d946
Jonas_Nunn <jonasnunn@gmail.com>
2023-03-08 13:35:35 -0700

    added dynamic weather

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -25,16 +25,33 @@ if (day == 1 || day == 5){
     document.querySelector("#banner").style.display = "block";
 }
 
-// Windchill calculations
-const temp = document.querySelector(".temp").textContent;
-const wSpeed = document.querySelector(".wind-speed").textContent;
+localStorage.setItem("name", "Jonas Nunn - Permanent")
+sessionStorage.setItem("Current Class", "WDD 230 - not Permanent")
 
+
+
+// Windchill calculations
+const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/allen%2C%20tx?unitGroup=us&key=5WRNLWGHZZSTGCT4HG57TDCM6&contentType=json";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    let t = data.currentConditions.temp;
+    document.querySelector('#t').textContent = t.toFixed(1);
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+    document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+    document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+    document.querySelector('#weather_icon').src = image;
+    document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
+    console.log(data);
+  };
+getWeather();
+
+
+const temp = document.querySelector("#t").textContent;
+const wSpeed = document.querySelector("#ws").textContent;
 let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (.04275*temp*Math.pow(wSpeed, 0.16)));
 
 if (temp <= 50 && wSpeed >= 3) {
     const windChill = document.querySelector(".wind-chill")
     windChill.textContent = chill;
 }
-
-localStorage.setItem("name", "Jonas Nunn - Permanent")
-sessionStorage.setItem("Current Class", "WDD 230 - not Permanent")
