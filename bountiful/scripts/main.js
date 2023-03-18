d1c916ad57dd6412d97f8c438b1e3310c1edd427
Jonas_Nunn <jonasnunn@gmail.com>
2023-03-18 15:36:22 -0600

    bountiful foods

--- bountiful/scripts/main.js
+++ bountiful/scripts/main.js
@@ -1,1 +1,33 @@
 // console.log("hi")
+nav = document.querySelector("#nav");
+hamButton = document.querySelector("#ham-icon");
+hamButton.addEventListener("click", ()=> {
+    // nav.style.flex.direction = "column";
+})
+
+
+// Windchill calculations
+const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/carlsbad?unitGroup=us&key=5WRNLWGHZZSTGCT4HG57TDCM6&contentType=json";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    let t = data.currentConditions.temp;
+    document.querySelector('#t').textContent = t.toFixed(1);
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+    document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+    document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+    document.querySelector('#weather_icon').src = image;
+    document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
+    // console.log(data);
+  };
+getWeather();
+
+
+const temp = document.querySelector("#t").textContent;
+const wSpeed = document.querySelector("#ws").textContent;
+let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (.04275*temp*Math.pow(wSpeed, 0.16)));
+
+if (temp <= 50 && wSpeed >= 3) {
+    const windChill = document.querySelector(".wind-chill")
+    windChill.textContent = chill;
+}
